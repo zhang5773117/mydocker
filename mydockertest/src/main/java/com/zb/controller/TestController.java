964feb120e021a33c4672d13fa7c754adbdca521
zhangjian <279694714@qq.com>
2019-05-22 14:37:14 +0800

    init

---
+++ mydockertest/src/main/java/com/zb/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.zb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/show/{name}")
+    public String show(@PathVariable("name") String name){
+        return "hello:"+name;
+    }
+}
